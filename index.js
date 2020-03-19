b225b44c68a0e6c8a3c30b521107da3ec04f1eb1
Celia <celiafrom12@gmail.com>
2020-03-19 11:09:29 +0000

    js

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 window.addEventListener('load', () => {
   const sounds = document.querySelectorAll('sound');
   const pads = document.querySelectorAll('pads.div');
-
+  const visual = document.querySelectorAll('.visuals')
+  const colors = ['#60d394', '#d36060', '#c060d3', '#d3d160', '#6860d3', '#60b2d3']
 
   //sound
 
-  pads.forEach(pad => {
+  pads.forEach((pad, index) => {
     pad.addEventListener('click', function(){
-      sounds[0]
+      sounds[index].currentTime = 0;
+      sounds[index].play
     });
   });
 });
